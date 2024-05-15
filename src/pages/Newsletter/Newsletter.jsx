402dc95bda8c957911c4e12e3fdd37613f5bb686
Sangma83 <poushalisangma13@gmail.com>
2024-05-15 22:44:01 +0600

    env variable set

--- src/pages/Newsletter/Newsletter.jsx
+++ src/pages/Newsletter/Newsletter.jsx
@@ -1,7 +1,28 @@
+import { ToastContainer, toast } from 'react-toastify';
 import brush from '../../assets/501.jpg';
 import book from '../../assets/h1-img-01.png';
+import { useState } from 'react';
 
 const Newsletter = () => {
+   const [subscribe, setSubscribe] = useState(false);
+
+    
+    const handleSubscribe = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        console.log( name, email);
+
+        if (!subscribe) {
+            toast("Thank you for subscribing to our newsletter!");
+            setSubscribe(true);
+            // Clear form fields after subscribing
+            form.name.value = '';
+            form.email.value = '';
+         }
+       
+    }
     return (
         <div className=''>
              <img className='h-[380px] w-full' src={brush} alt="" />
@@ -11,11 +32,12 @@ const Newsletter = () => {
                     <h2 className='font-bold text-black text-3xl'>FINDING THE PERFECT TRAILS TO HIKE IS EASY WITH <span className='text-lime-700'>NEWSLETTER</span></h2>
 
                     <div className="join mt-8">
-                     <input className="input input-bordered join-item" placeholder="Name"/>
-                     <input className="input input-bordered join-item" placeholder="Email"/>
-                       <button className="btn btn-primary join-item rounded-r-full">Subscribe</button>
+                     <input className="input input-bordered join-item" name='name' placeholder="Name"/>
+                     <input className="input input-bordered join-item" name='email' placeholder="Email"/>
+                       <button onClick={handleSubscribe} className="btn btn-primary join-item rounded-r-full">Subscribe</button>
                           </div>
                 </div>
+                <ToastContainer></ToastContainer>
             </div>
         </div>
     );
